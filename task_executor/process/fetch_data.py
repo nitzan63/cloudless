9a7210560b8acd0c74eec9222fd1d93d4b8e4bc2
AradTenenbaum <arad.tenen@gmail.com>
2025-05-19 17:11:21 +0300

    docker compose updates, dockerize task executor

--- task_executor/process/fetch_data.py
+++ task_executor/process/fetch_data.py
@@ -1,23 +1,26 @@
 import requests
 import os
 from dotenv import load_dotenv
-load_dotenv()
+import logging
 
+load_dotenv()
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 DATA_SERVICE_URL = f"{os.environ.get('DATA_SERVICE_URL')}"
+SHARED_SCRIPTS_PATH = f"{os.environ.get('SHARED_SCRIPTS_PATH')}"
 
 def fetch_task(task_id):
     response = requests.get(f"{DATA_SERVICE_URL}/tasks/exec/{task_id}")
 
     file_data = response.json()
-    print(file_data)
+    logger.info(f"File data: {file_data}")
     filename = None
 
     if response.status_code == 200:
         filename = file_data['file_name']
-        dir_name = "files"
-        os.makedirs(dir_name, exist_ok=True)
-        file_path = os.path.join(dir_name, filename)
+        os.makedirs(SHARED_SCRIPTS_PATH, exist_ok=True)
+        file_path = os.path.join(SHARED_SCRIPTS_PATH, filename)
 
         with open(file_path, "w") as f:
             f.write(file_data['file_content'])
--- task_executor/process/processor.py
+++ task_executor/process/processor.py
@@ -1,8 +1,12 @@
 from process.fetch_data import fetch_task
 from process.execute import submit_to_spark
+import logging
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def process(task_id: str):
-    print("TASK_ID:", task_id)
+    logger.info(f"------------Start Processing {task_id}------------")
     filename = fetch_task(task_id)
     submit_to_spark(filename)
+    logger.info(f"------------End Processing {task_id}------------")
